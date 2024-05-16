57c656e6f4004fbfd8f486b9207890d3cb00a91f
Sarah Roperch <131868257+SarahRoperch@users.noreply.github.com>
2024-05-16 13:05:14 +0200

    update port on server

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.use(jwtMiddleware);
 app.use("/api", playerRoutes);
 
 // Démarrage du serveur
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
