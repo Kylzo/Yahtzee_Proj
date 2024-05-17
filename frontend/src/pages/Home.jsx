d68f533d5177b124e64e81f134eb4573d837f92c
Theomrh <92977368+Theomrh@users.noreply.github.com>
2024-05-17 11:36:54 +0200

    Create home page and jeu page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,38 +1,21 @@
-import { useState } from "react";
-import PopupChat from "../components/PopupChat";
-import RollDice from "../components/RollDice";
 import "../styles/Home.css";
 
 const Home = () => {
-  const [openChat, setOpenChat] = useState(false);
-
-  const openPopupChat = () => {
-    setOpenChat(!openChat);
-  };
-
-  const closePopupChat = () => {
-    setOpenChat(false);
-  };
-
   return (
     <main className="home">
-      {/* Button paramètres */}
-      <div className="container-action-btn">
-        <button className="action-btn">
-          <i className="fa-solid fa-gear"></i>
-        </button>
-        <button onClick={openPopupChat} className="action-btn">
-          <i className="fa-solid fa-comments"></i>
-        </button>
+      <div className="home-group">
+        <h1>Yahtzee</h1>
+        <p className="home-description">
+          Venez jouer au meilleur jeu de tous les temps !
+          <br />
+          Vous ne serez absolument pas déçu et vivrez une expérience incroyable
+          !
+        </p>
+        <div className="home-container-button">
+          <button>Jouer en multijoueur</button>
+          <button>Jouer en solo</button>
+        </div>
       </div>
-
-      {/* Les dés + le boutton pour lancer les dés */}
-      <div>
-        <RollDice />
-      </div>
-
-      {/* Chat */}
-      <PopupChat openChat={openChat} closePopupChat={closePopupChat} />
     </main>
   );
 };
