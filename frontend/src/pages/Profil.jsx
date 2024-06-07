7e5ce0e9271f62e8a685a1332ca908782f80a206
Sarah Roperch <131868257+SarahRoperch@users.noreply.github.com>
2024-06-07 19:25:09 +0200

    update profile

--- frontend/src/pages/Profil.jsx
+++ frontend/src/pages/Profil.jsx
@@ -1,15 +1,24 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/Profil.css";
+import { useAuth } from "../context/AuthContext.js";
 
 const Profil = () => {
   const [user, setUser] = useState(null);
   const [formData, setFormData] = useState({});
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const [popupDelete, setPopupDelete] = useState(false);
+  const navigate = useNavigate();
+  const { setIsAuthenticated } = useAuth();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value); // Met à jour l'état du mot de passe de confirmation
   };
 
   const handleSubmit = async (e) => {
@@ -38,14 +47,12 @@ const Profil = () => {
         return;
       }
 
-      // Requête pour récupérer les données mises à jour de l'utilisateur
       const response = await fetch("http://localhost:3000/api/current-user", {
         method: "GET",
         credentials: "include",
       });
       const userData = await response.json();
 
-      // Mettre à jour l'état de l'utilisateur avec les nouvelles données
       setUser(userData);
 
       setLoading(false);
@@ -57,6 +64,52 @@ const Profil = () => {
 
   const toggle = () => {
     setPopupDelete(!popupDelete);
+  };
+
+  const handleLogOut = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/api/logout", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      if (res.ok) {
+        setIsAuthenticated(false);
+        navigate("/connexion");
+      } else {
+        console.error("Échec de la déconnexion");
+      }
+    } catch (error) {
+      console.error("Erreur lors de la déconnexion:", error);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      const res = await fetch(
+        `http://localhost:3000/api/player?id_player=${user.id_player}`,
+        {
+          method: "DELETE",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ password }),
+        }
+      );
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+
+      await handleLogOut();
+    } catch (error) {
+      console.error("Erreur lors de la suppression du compte :", error);
+      setError(true);
+    }
   };
 
   useEffect(() => {
@@ -126,10 +179,19 @@ const Profil = () => {
             <div className="container-modale">
               <div className="modale-delete-account">
                 <p className="title-delete">
-                  Êtes vous sûr de vouloir supprimer votre compte ?
+                  Êtes-vous sûr de vouloir supprimer votre compte ?
                 </p>
                 <div className="container-btn">
-                  <button>Oui, je suis sûr</button>
+                  <label htmlFor="confirm-password">
+                    Confirmez votre mot de passe :
+                  </label>
+                  <input
+                    id="confirm-password"
+                    name="confirm-password"
+                    type="password"
+                    onChange={handlePasswordChange}
+                  />
+                  <button onClick={handleDelete}>Oui, je suis sûr</button>
                   <button onClick={toggle}>Non, annuler</button>
                 </div>
               </div>
